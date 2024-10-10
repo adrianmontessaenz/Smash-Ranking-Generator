7852df8cc8247fa51bee485b976177990f2587c9
Adrián Montes <adrianmontessaenz@outlook.com>
2024-10-10 17:22:27 +0200

    Started with H2H but only when creating new file. Still working on it

--- src/app.py
+++ src/app.py
@@ -42,36 +42,36 @@ def fetch_tournament(slug, eventSlug):
 
     # Query to get standings
     standings_query = gql("""
-    query getStandings($slug: String!, $perPage: Int!, $page: Int!, $eventSlug: String!) {
+    query getStandings($slug: String!, $perPage: Int!, $page: Int! $eventSlug: String!) {
       tournament(slug: $slug) {
         name
         events(filter: {slug: $eventSlug}) {
-          entrants(query:{})  {
-          	pageInfo {
+          entrants(query:{perPage: $perPage, page: $page})  {
+            pageInfo {
             	total
           	}
-        	}
-          standings(query: { perPage: $perPage, page: $page }) {
             nodes{
-              placement
-              entrant
-              {
-                id
-                name
-                paginatedSets
-                {
-                	nodes
-                  {
-                    displayScore
-                    winnerId
-                  }
-                }
-              }
-              player
-              {
+              id
+              participants{
                 gamerTag
               }
-            }
+              standing {
+                placement
+              }
+              paginatedSets {
+                nodes
+                {
+                  winnerId
+                  slots{
+                    entrant{
+                      participants{
+                        gamerTag
+                      }
+                    }
+                  }                
+                }
+            	}
+        		}
           }
         }
       }
@@ -79,14 +79,17 @@ def fetch_tournament(slug, eventSlug):
     """)
     
     # Get placements and join jsons
-    total_entrants_reduced = int(total_entrants / 4)
+    tmp_counter = 0
+    tmp_page_counter = 1
     final_json = []
-    for tmp in range(5):
-      json_data = client.execute(standings_query, variable_values={"slug": slug, "perPage": total_entrants_reduced, "page": tmp + 1, "eventSlug": eventSlug})
+    while tmp_counter < total_entrants:
+      json_data = client.execute(standings_query, variable_values={"slug": slug, "perPage": 10, "page": tmp_page_counter, "eventSlug": eventSlug})
       if not final_json:
         final_json = json_data
       else:
-        final_json['tournament']['events'][0]['standings']['nodes'] += json_data['tournament']['events'][0]['standings']['nodes']
+        final_json['tournament']['events'][0]['entrants']['nodes'] += json_data['tournament']['events'][0]['entrants']['nodes']
+      tmp_counter += 10
+      tmp_page_counter += 1
     return final_json
 
 if __name__ == "__main__":
@@ -126,33 +129,56 @@ if __name__ == "__main__":
       # Get required data separately and create new column for tournament
       players_column = tournament_df['Players']
       tournament_name = final_data['tournament']['name']
-      entrants = final_data['tournament']['events'][0]['standings']['nodes']
+      entrants = final_data['tournament']['events'][0]['entrants']['nodes']
       tournament_df[tournament_name] = None
       
       # Set placements for new players and players that already participated in previous tournaments
       for entrant in entrants:
-        player_name = entrant['player']['gamerTag']            
+        player_name = entrant['participants'][0]['gamerTag']            
         player_idx = 0
         # Append new player to the 'Players' column if it doesn't exist
         if player_name not in players_column.values:
-          new_row = {'Players': player_name, tournament_name: entrant['placement']}
+          new_row = {'Players': player_name, tournament_name: entrant['standing']['placement']}
           tournament_df.loc[len(tournament_df)] = new_row
         else:
           player_idx = players_column[players_column == player_name].index[0]
-          tournament_df.iloc[player_idx, column_index] = entrant['placement']    
+          tournament_df.iloc[player_idx, column_index] = entrant['standing']['placement']    
       
     # If first time writing on excel
     else:
-      # Write excel
+      # Write excel sheet for placements
       tournament_name = final_data['tournament']['name']
-      entrants = final_data['tournament']['events'][0]['standings']['nodes']
-
+      entrants = final_data['tournament']['events'][0]['entrants']['nodes']
+      playerNames = [entrant['participants'][0]['gamerTag'] for entrant in entrants]
+      placements = [entrant['standing']['placement'] for entrant in entrants]
       index = ['Players', tournament_name]
+      
       # Create a dictionary for DataFrame
-      data = {entrant['player']['gamerTag']: entrant['placement'] for entrant in entrants}
-
-      tournament_df = pd.DataFrame(list(data.items()), columns=index)
-
+      tournament_df = pd.DataFrame({'Players': playerNames, tournament_name: placements})
+      
+      #Write excel sheet for head to heads
+      head2head_df = pd.DataFrame(index=playerNames, columns=playerNames)
+      head2head_df = head2head_df.fillna(0)
+      
+      # Fill diagonal with '-'
+      for player in playerNames:
+        head2head_df.loc[player, player] = '-'
+        
+      # Check matches now
+      for entrant in entrants:
+        gamerId = entrant['id']
+        gamerTag = entrant['participants'][0]['gamerTag']
+        for set in entrant['paginatedSets']['nodes']:
+          
+          # If player won set, mark head to head as win
+          if set['winnerId'] == gamerId:
+            player1name = set['slots'][0]['entrant']['participants'][0]['gamerTag']
+            player2name = set['slots'][1]['entrant']['participants'][0]['gamerTag']
+            rivalName = player1name if player1name != gamerTag else player2name
+            
+            # Get player row and rival column and add
+            head2head_df.loc[gamerTag, rivalName] += 1
+            
     # Write the DataFrames to different sheets in an Excel file
     with pd.ExcelWriter('ranking_data.xlsx') as writer:
       tournament_df.to_excel(writer, sheet_name='Placements', index=False)
